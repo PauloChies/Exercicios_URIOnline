d6d3b839ed0961b4afa7ebd386b8b44764eaf1f3
Paulo Chies <paulo.chies@outlook.com>
2018-10-07 22:25:49 -0300

    1001

---
+++ 1001.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+     Scanner ler = new Scanner(System.in);
+     int a, b, soma;
+     a = ler.nextInt();
+     b = ler.nextInt();
+     soma = a+b;
+     System.out.println("X = " + soma);  
+    }
+
+}
